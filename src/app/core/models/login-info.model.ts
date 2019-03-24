cef4f77bdb983bcbb262bbc8576b6a93133be40e
Hoang Linh <truonghoanglinhk55bkhn@gmail.com>
2019-03-24 11:22:18 +0700

    - Remove redundant codes

--- src/app/core/models/login-info.model.ts
+++
@@ -1,8 +0,0 @@
-export class LoginInfo {
-    constructor(public title: string,
-                public emailOrUsername: string,
-                public password: string,
-                public website: string,
-                public createdAt: Date,
-                public modifiedAt: Date) {}
-}
