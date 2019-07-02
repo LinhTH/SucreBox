e724854a7792091ca16bad87c213514fe374bbe6
Hoang Linh <truonghoanglinhk55bkhn@gmail.com>
2019-07-03 06:01:54 +0700

    feat: show the number of password

--- src/app/modules/password-count/containers/password-count-page.component.ts
+++ src/app/modules/password-count/containers/password-count-page.component.ts
@@ -1,7 +1,22 @@
+import { PasswordInfo } from './../../../core/models/password-info.model';
+import { PasswordListSelector } from './../../password-overview/store/selectors/password-list.selector';
+import { Store, select } from '@ngrx/store';
 import { Component } from '@angular/core';
+import { AppState } from '../../../core/store/app.store';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'sucre-password-count-page',
   templateUrl: './password-count-page.component.html'
 })
-export class PasswordCountPageComponent {}
+export class PasswordCountPageComponent {
+  passwordCount$: Observable<number> = this.store.pipe(
+    select(PasswordListSelector.getPasswordList),
+    map((passwordList: PasswordInfo[]) => {
+        return _.size(passwordList);
+    })
+  );
+  constructor(private store: Store<AppState>) {}
+}
